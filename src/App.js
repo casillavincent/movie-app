6ffc95129207b703b392f23a984a9f3dcf84ac5a
Vincent Casilla <73447863+mikanunu@users.noreply.github.com>
2021-01-10 18:34:36 -0800

    Initial Commit (React Dev Files)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+function App() {
+   return (
+      <React.Fragment>
+         <h1> Main render</h1>
+         <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi veritatis consequatur
+            incidunt repellat, minima ipsum sequi tenetur sapiente ipsam alias ab, iusto corrupti,
+            amet beatae eius delectus ullam voluptas quia?
+         </p>
+      </React.Fragment>
+   );
+}
+
+export default App;
